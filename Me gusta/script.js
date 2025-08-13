14d57c28d5d3da8d5cc74bb91e30a7b10d80cd09
akv-gami <tellusin@gmail.com>
2025-08-13 17:21:07 -0400

    Me gusta Core añadido

---
+++ Me gusta/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const likeButtons = document.querySelectorAll('.like-button');
+
+    likeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const postActions = button.closest('.post-actions');
+            const likeCountElement = postActions.querySelector('.like-count');
+            
+            let currentLikes = parseInt(likeCountElement.innerText);
+            currentLikes++;
+            
+            likeCountElement.innerText = `${currentLikes} like(s)`;
+        });
+    });
+});
